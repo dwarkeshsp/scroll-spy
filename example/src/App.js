7aabbee243c4fa803ce570732bbdab7e525dbfb9
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2020-12-01 13:24:50 -0600

    dynamic url and highlighting; links work

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
-import { ExampleComponent } from 'scroll-spy'
-import 'scroll-spy/dist/index.css'
+import { Canvas, Menu, MenuItem, Content } from 'scroll-spy'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <Canvas>
+      <Menu>
+        <MenuItem title='Hey' toId='alpha' />
+        <MenuItem title='Ho' toId='beta' />
+      </Menu>
+      <Content>
+        <Section name='alpha'></Section>
+        <Section name='beta'></Section>
+      </Content>
+    </Canvas>
+  )
+}
+
+function Section({ name }) {
+  return (
+    <div
+      id={name}
+      style={{
+        height: '100vh'
+      }}
+    >
+      {name}
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,66 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Canvas = (props) => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      {props.children}
+    </div>
+  )
+}
+
+export const Menu = (props) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '15%',
+        position: 'fixed',
+        top: ''
+      }}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+export const MenuItem = ({ title, toId }) => {
+  const [highlight, setHighlight] = React.useState(false)
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setHighlight(entry.isIntersecting)
+      },
+      { threshold: 0.5 }
+    )
+    observer.observe(document.getElementById(toId))
+
+    if (highlight) {
+      window.history.replaceState(null, null, '#' + toId)
+    }
+
+    return () => {
+      observer.unobserve(document.getElementById(toId))
+    }
+  })
+
+  function scrollToId() {
+    document.getElementById(toId).scrollIntoView()
+  }
+
+  return (
+    <div
+      style={{
+        backgroundColor: highlight ? '#808080' : '#A9A9A9'
+      }}
+      onClick={scrollToId}
+    >
+      {title}
+    </div>
+  )
+}
+
+export const Content = (props) => {
+  return <div style={{ width: '85%', marginLeft: '15%' }}>{props.children}</div>
 }
